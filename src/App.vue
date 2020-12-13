2ffacc0852caf4e3ec34355deda3de4fc1781abb
= <=>
2020-12-13 19:18:05 +0300

    Initial boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav id="nav">
+    <h1>Todos</h1>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+  </nav>
   <router-view />
 </template>
 
 <style>
+:root {
+  --black: #2c3e50;
+  --primary-color: #42b983;
+  --primary-constrast: var(--black);
+  font-size: 20px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-weight: 300;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: var(--black);
 }
 
 #nav {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   padding: 30px;
 }
 
+#nav h1 {
+  font-size: 2.5rem;
+}
+
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  font-weight: 300;
+  color: var(--black);
+  text-decoration: none;
+  font-size: 1.2rem;
+}
+
+#nav ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin-top: 10px;
+}
+
+#nav li {
+  display: inline-block;
+  margin: 0 10px;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: var(--primary-color);
 }
 </style>
